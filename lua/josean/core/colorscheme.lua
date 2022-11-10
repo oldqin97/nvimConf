ef0d9125920c212f3bedf8ed0fce2335dba82a2f
oldqin <oldqin1997@gmail.com>
2022-11-11 02:04:31 +0800

    修改commnet颜色

--- lua/josean/core/colorscheme.lua
+++ lua/josean/core/colorscheme.lua
@@ -23,15 +23,11 @@ require("github-theme").setup({
   sidebars = { "qf", "vista_kind", "terminal", "packer" },
 
   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = { hint = "orange", error = "#ff0000" },
 
   -- Overwrite the highlight groups
   overrides = function(c)
     return {
-      htmlTag = { fg = c.red, bg = "#282c34", sp = c.hint, style = "underline" },
-      DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-      -- this will remove the highlight groups
-      TSField = {},
+      Comment = { fg = "#82e0aa" },
     }
   end,
 })
